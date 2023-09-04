5f78789f78b0ea14e274182d135a2e1ce2bf6169
André Franciosi <andref2210@hotmail.com>
2023-09-04 20:43:13 -0300

    Animações
    Adicionando animações nos botões

--- app/src/main/java/com/example/app_pde/MainActivity.java
+++ app/src/main/java/com/example/app_pde/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.app_pde;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,14 +15,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button botao = findViewById(R.id.botao_inicial);
+        final Button botao = findViewById(R.id.botao_inicial);
+        final Animation scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.scale_animation);
+
         botao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Criar uma Intent para iniciar a segunda atividade
-                Intent intent = new Intent(MainActivity.this, SegundaActivity.class);
-                startActivity(intent);
+                // Aplica a animação ao botão clicado
+                botao.startAnimation(scaleAnimation);
+
+                // Define um AnimationListener para executar a ação após a animação
+                scaleAnimation.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        // Ação a ser executada quando a animação começa (opcional)
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        // Ação a ser executada após o término da animação
+                        iniciarSegundaActivity(); // Função para iniciar a próxima atividade
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                        // Ação a ser executada quando a animação se repete (opcional)
+                    }
+                });
             }
         });
     }
+
+    private void iniciarSegundaActivity() {
+        // Criar uma Intent para iniciar a segunda atividade
+        Intent intent = new Intent(MainActivity.this, SegundaActivity.class);
+        startActivity(intent);
+    }
 }
+
--- app/src/main/java/com/example/app_pde/SegundaActivity.java
+++ app/src/main/java/com/example/app_pde/SegundaActivity.java
@@ -1,6 +1,10 @@
 package com.example.app_pde;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SegundaActivity extends AppCompatActivity {
@@ -9,5 +13,25 @@ public class SegundaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        final Button botao1 = findViewById(R.id.botao1);
+        final Button botao2 = findViewById(R.id.botao2);
+        final Button botao3 = findViewById(R.id.botao3);
+        final Button botao4 = findViewById(R.id.botao4);
+
+        final Animation scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.scale_animation);
+
+        View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Aplica a animação ao botão clicado
+                view.startAnimation(scaleAnimation);
+            }
+        };
+
+        botao1.setOnClickListener(clickListener);
+        botao2.setOnClickListener(clickListener);
+        botao3.setOnClickListener(clickListener);
+        botao4.setOnClickListener(clickListener);
     }
 }
